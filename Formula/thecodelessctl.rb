cd9590fcfc6ff427eee8f6a4bcbcff5daa740606
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-14 14:07:22 -0400

    Brew formula update for thecodelessctl version v0.0.6

--- Formula/thecodelessctl.rb
+++ Formula/thecodelessctl.rb
@@ -5,24 +5,24 @@
 class Thecodelessctl < Formula
   desc ""
   homepage ""
-  version "0.0.5"
+  version "0.0.6"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/jasonblanchard/thecodelessctl/releases/download/v0.0.5/thecodelessctl_0.0.5_Darwin_x86_64.tar.gz"
-    sha256 "6397adf094b39cbf9547dbbf288a505a96ae944ce85d74e1699ab69ec02ff8db"
+    url "https://github.com/jasonblanchard/thecodelessctl/releases/download/v0.0.6/thecodelessctl_0.0.6_Darwin_x86_64.tar.gz"
+    sha256 "1e10c3392abdd386e74170548cd0dcf7266f23047814fbdefb04669ad31b4e2c"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/jasonblanchard/thecodelessctl/releases/download/v0.0.5/thecodelessctl_0.0.5_Darwin_arm64.tar.gz"
-    sha256 "2a53227ef43707e299ed9f84962ccddf575108f9aec9684c79b1e5f6004a5ea3"
+    url "https://github.com/jasonblanchard/thecodelessctl/releases/download/v0.0.6/thecodelessctl_0.0.6_Darwin_arm64.tar.gz"
+    sha256 "93666831f0f216b97411c924260235de8dba8a3f12fdad1d7bb8d17cd9bff6fb"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jasonblanchard/thecodelessctl/releases/download/v0.0.5/thecodelessctl_0.0.5_Linux_x86_64.tar.gz"
-    sha256 "c2ce47c187c0b5be873d7db917d0f3ab8325891eb1cc64ef1b405888f6800cb7"
+    url "https://github.com/jasonblanchard/thecodelessctl/releases/download/v0.0.6/thecodelessctl_0.0.6_Linux_x86_64.tar.gz"
+    sha256 "17875134bc9248033c5c75d8d4d441479a7fc37f42b26f49b25bff607643d3bf"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/jasonblanchard/thecodelessctl/releases/download/v0.0.5/thecodelessctl_0.0.5_Linux_arm64.tar.gz"
-    sha256 "21f2cb2075879f717b9264e18f565f2a2b7c04654abf87e159e7fdde3892d4ec"
+    url "https://github.com/jasonblanchard/thecodelessctl/releases/download/v0.0.6/thecodelessctl_0.0.6_Linux_arm64.tar.gz"
+    sha256 "2667abbde94e2c769ae4b7b3e79b283346a0978ed368d5b8651d0ebe47054a1a"
   end
 
   def install
